7b178329a192cf86dec72dca79016c5fc1ea1710
ByeongGwon <lee971009@naver.com>
2022-04-05 17:47:06 +0900

    Gold 3

---
+++ 2216.py
@@ -0,0 +1,22 @@
+a, b, c = map(int, input().split())
+X = input()
+Y = input()
+
+dp = [[0]*(len(X)+1) for i in range(len(Y)+1)]
+
+print(dp)
+for i in range(1, len(X)+1):
+    dp[0][i] = dp[0][i-1] + b
+for i in range(1, len(Y)+1):
+    dp[i][0] = dp[i-1][0] + b
+
+print(dp)
+for i in range(1, len(Y)+1):
+    for j in range(1, len(X) + 1):
+        if X[j-1] == Y[i-1]:
+            dp[i][j] = dp[i-1][j-1] + a
+        else:
+            dp[i][j] = max(dp[i-1][j-1] + c, dp[i-1]
+                           [j] + b, dp[i][j-1] + b)
+print(dp)
+print(dp[len(Y)][len(X)])
