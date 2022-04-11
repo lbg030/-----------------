9c9ff41db92dea8d796124d89d1f718eb41c44e2
ByeongGwon <lee971009@naver.com>
2022-04-11 22:30:40 +0900

    2346 ~ing

--- 연결리스트/2346.py
+++ 연결리스트/2346.py
@@ -1,22 +1,20 @@
 n = int(input())
 li = list(map(int, input().split()))
 li2 = li[:]
-
 count_list = []
-
 idx = 0
-for _ in range(n):
-    a = li[idx]
-    del li[idx]
-    idx += a
 
-    count_list.append(li2.index(a) + 1)
-    print(f"a = {a}, li2.index(a) = {li2.index(a)}")
 
-    if(idx > len(li)):
+for _ in range(n-1):
+    value = li[idx]
+    temp = idx
+    count_list.append(li2.index(value) + 1)
+    print(f"value = {value}")
+    idx += value
+    del li[temp]
+    while idx >= len(li):
+        print(";;")
         idx -= len(li)
 
-
+count_list.append(*li)
 print(*count_list)
-
-# 1 4 5 3 2
