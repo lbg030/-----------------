1debc5ce710cedf1ecd04b41fcb6868f3863f2d2
ByeongGwon <lee971009@naver.com>
2022-04-12 18:33:23 +0900

    Silver 3

--- 연결리스트/5397.py
+++ 연결리스트/5397.py
@@ -3,10 +3,24 @@ n = int(input())
 
 for _ in range(n):
     li = []
-    li.append(list(input()))
+    li.extend(list(map(str, input())))
     idx = 0
+    ans = []
+    # print(li)
     for j in range(len(li)):
-        if(li[j] == '<' and idx > 0):
-            idx -= 1
-        if(li[j] == '>' and idx < len(li) - 1):
+        if(li[j] == '<'):
+            if(idx > 0):
+                idx -= 1
+        elif(li[j] == '>'):
+            if(idx < len(li) - 1):
+                idx += 1
+
+        elif(li[j] == '-'):
+            if(li[j] != '>' or li[j] != '<'):
+                ans.pop()
+
+        else:
+            ans.insert(idx, li[j])
             idx += 1
+
+    print(*ans, sep='')
