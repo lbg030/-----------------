24c6c7f7858501c87f01e71249117d86fa8255d1
ByeongGwon <lee971009@naver.com>
2022-04-23 13:39:18 +0900

    Gold 4

---
+++ Stack/Queue/16120.py
@@ -0,0 +1,21 @@
+# p -> ppap로 바뀐것이기 때문에 결국 ppap -> p로 다시 바꾸어 준다면
+# 이 문자열이 ppap로 구성되어 있는지 아닌지 확인 가능하다.
+
+# pppapap -> pPap [ P = ppap ]
+# ppapapp -> Papp [ P = ppap ] 이런식으로
+
+w = input()
+stack = []
+ppap = ["P", "P", "A", "P"]
+for i in range(len(w)):
+    stack.append(w[i])
+    print(stack)
+    if stack[-4:] == ppap:
+        for _ in range(4):
+            stack.pop()
+        stack.append("P")
+
+if stack == ppap or stack == ["P"]:
+    print("PPAP")
+else:
+    print("NP")
