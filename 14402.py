9665942d3ff868f3e17c77e06df5b49d2e65e145
ByeongGwon <lee971009@naver.com>
2022-05-10 00:45:17 +0900

    Changes

--- 14402.py
+++ 14402.py
@@ -1,4 +1,25 @@
-import sys
-# sys.stdin = open('input.txt', 'r')
-a, b = input().split()
-print(int(a) * int(b))
+from sys import stdin
+
+n = int(input())
+dic = {}
+cnt = 0
+for _ in range(n):
+    name, work = stdin.readline().rstrip().split()
+
+    if work == '-':
+        if name not in dic or dic[name] == 0:
+            cnt += 1
+        elif name in dic:
+            dic[name] -= 1
+
+    #work = +
+    else:
+        if name not in dic:
+            dic[name] = 1
+        else:
+            dic[name] += 1
+
+if len(dic) == 0:
+    print(cnt)
+else:
+    print(sum(dic.values()) + cnt)
